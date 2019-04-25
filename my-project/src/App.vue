141c2b0fbfed8d0cc41b7ff6df3001813e466b19
AnnaTarrach <tarrach.anna@gmail.com>
2019-04-25 14:49:46 +0200

    Website

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -1,6 +1,63 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <h1>Unser tolles Projekt</h1>
+  <div class="project">
+    <div class="title">
+      <img class="image" src="./assets/Titelbild.png" alt="Schwangerschaft">
+      <h1>Unser tolles Projekt + Bild</h1>
+        <div class="title-content">
+          <p>Hier Text, Einleitung etc.</p>
+        </div>
+    </div>
+    <div class="content-white">
+      <p><b>Hier Inhalt einfügen</b>
+      <br>und tolle Diagramme</p>
+    </div>  
+    <div class="content-grey">
+      <p><b>Hier Inhalt einfügen</b>
+      <br>und tolle Diagramme</p>
+    </div>
+    <div class="content-white">
+      <p><b>Hier Inhalt einfügen</b>
+      <br>und tolle Diagramme</p>
+    </div> 
+    <div class="footer">
+      <p>Ein Projekt von Grit Ludwig, Anne Westphal & Anna Tarrach</p>
+    </div>
   </div>
+
 </template>
+
+<script>
+export default {
+  
+}
+</script>
+
+
+<style>
+  html, body {
+    font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  }
+  .title {
+    text-align: center;
+    background-color:rgb(216, 238, 238);
+    padding: 30px;
+  }
+  .image {
+    width:70%;
+  }
+  .content-white {
+    background-color: white;
+    padding: 30px;
+
+  }
+  .content-grey {
+    background-color:rgb(240, 240, 240);
+    padding: 30px;
+  }
+  .footer {
+    font-size: 8pt;
+    color:rgb(107, 107, 107);
+    text-align:center;
+    padding: 30px;
+  }
+</style>
